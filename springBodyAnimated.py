6f98f3868f4a2582515819c2fb05ea10ba0b3dfd
gusals3587 <gusals3587@gmail.com>
2019-01-12 20:15:14 +0900

    initial working prototype, still working on the xlim dynamic reinitialization

---
+++ springBodyAnimated.py
@@ -0,0 +1,72 @@
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+import itertools
+import unittest
+
+def main():
+    fig, ax = plt.subplots()
+    weight = Body(y = 1.0, v = 0.0, dt = 0.1, ax = ax)
+    anim = FuncAnimation(fig, weight, frames=itertools.count(), init_func=weight.init)
+    plt.show()
+
+class Body():
+
+
+    def __init__(self, y, v, dt, ax):
+        # t is the x-axis, y is the y-axis
+        self.y = [y]
+        self.v = v
+        self.a = -y
+        self.t = [0]
+        self.dt = dt
+        self.ax = ax
+        self.line, = ax.plot([], [])
+
+        self.ax.set_ylim(-1, 1)
+        self.ax.set_xlim(0, 5)
+
+        self.counter = 30
+
+    def init(self):
+        self.line.set_data(self.t, self.y)
+        print(self.line.get_data()[1])
+        return self.line,
+
+    def __call__(self, i):
+        if i == 0:
+            self.v += (self.dt / 2) * self.a
+            self.t += [self.dt * i]
+        else:
+            self.v += + self.dt * self.a
+            self.t += [self.dt * i]
+        if self.counter == 0:
+            self.counter = 30
+            newXlim = i * 1.8 * self.dt
+            self.ax.set_xlim(right=newXlim)
+        self.y += [self.y[-1] + (self.dt * self.v)]
+        self.a = -self.y[-1]
+        self.line.set_data(self.t, self.y)
+        self.counter -= 1
+        return self.line,
+
+class TestSimulationMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.fig, self.ax = plt.subplots()
+        self.body = Body(y=1.0, v=0.0, dt=0.1, ax=self.ax)
+
+    def test_bodyInit(self):
+        testPlot = self.ax.plot(0.0, 1.0)[0]
+        dataPoint = self.body.init()[0]
+        self.assertEqual(testPlot.get_data(), dataPoint.get_data())
+
+    def test_bodyCall(self):
+        testPlot = self.ax.plot([0.0, 0.1], [1.0, 0.99])[0]
+        dataPoint = self.body(1)[0]
+
+        for i, j in zip(testPlot.get_data(), dataPoint.get_data()):
+            self.assertEqual(i[0], j[0])
+            self.assertEqual(i[1], j[1])
+
+if __name__ == "__main__":
+    main()
